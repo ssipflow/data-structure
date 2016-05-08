a729ef37a14150af68b8d707bea6674e9d2bdff7
ssipflow <ssipflow@gmail.com>
2016-05-08 14:38:40 +0900

    Added files via upload

---
+++ KevinBaconGame/ActorGraphNode.java
@@ -0,0 +1,40 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+//BFS
+public class ActorGraphNode {
+	private String name;
+	private Set<ActorGraphNode> linkedActors;
+	private int baconNumber = -1;
+	
+	public ActorGraphNode(String name){
+		this.name = name;
+		linkedActors = new HashSet<ActorGraphNode>();
+	}
+	
+	public void linkedCostar(ActorGraphNode costar){
+		linkedActors.add(costar);
+		costar.linkedActors.add(this);
+	}
+	
+	public int getBaconNumber(){
+		return baconNumber;
+	}
+	
+	public void setBaconNumbers(){
+		baconNumber = 0;
+		Queue<ActorGraphNode> queue = new LinkedList<ActorGraphNode>();
+		queue.add(this);
+		ActorGraphNode current;
+		while((current = queue.poll()) != null){
+			for(ActorGraphNode n : current.linkedActors){
+				if(n.baconNumber == -1){
+					n.baconNumber = current.baconNumber+1;
+					queue.add(n);
+				}
+			}
+		}
+	}
+}
