c90e6ed0d948090387b75d739ca4d09900889b6c
ssipflow <ssipflow@gmail.com>
2016-05-08 18:56:54 +0900

    Update FirstNonRepeat.java

--- FirstNonRepeatedChar/FirstNonRepeat.java
+++ FirstNonRepeatedChar/FirstNonRepeat.java
@@ -1,21 +1,22 @@
 import java.util.HashMap;
 
 public class FirstNonRepeat {
-	char FirstNonRepeat(String input){		
+	char FirstNonRepeatChar(String input){		
 		HashMap<Character, Integer> hash = new HashMap<Character, Integer>();
-				
-		int size = input.length();
+		int length = input.length();
 		char character;
-		for(int i = 0; i < size; i++){
+		
+		for(int i = 0; i < length; i++){
 			character = input.charAt(i);
 			if(hash.containsKey(character))
 				hash.put(character, hash.get(character)+1);
 			else
 				hash.put(character, 1);
 		}
-		for(int i = 0; i < size; i++){
+		
+		for(int i = 0; i < length; i++){
 			character = input.charAt(i);
-			if(hash.get(character)==1)
+			if(hash.get(character) == 1)
 				return character;
 		}
 		return 0;
@@ -25,7 +26,7 @@ public class FirstNonRepeat {
 		String input1 = "total";
 		String input2 = "teeter";
 		
-		System.out.println(input1 + "'s first non repeated character : " + new FirstNonRepeat().FirstNonRepeat(input1));
-		System.out.println(input2 + "'s first non repeated character : " + new FirstNonRepeat().FirstNonRepeat(input2));
+		System.out.println(input1 + "'s first non repeated character : " + new FirstNonRepeat().FirstNonRepeatChar(input1));
+		System.out.println(input2 + "'s first non repeated character : " + new FirstNonRepeat().FirstNonRepeatChar(input2));
 	}
 }
